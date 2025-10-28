16e70a3a0ec49427f3f71bd152913efe24ade32a
suzen1 <sayyedsuzenali@gmail.com>
2025-10-28 13:18:42 +0530

    new file:   Backend/src/Routes/chat.route.js 	modified:   Backend/src/Routes/user.route.js 	new file:   Backend/src/controllers/chat.controller.js 	modified:   Backend/src/controllers/user.controller.js 	new file:   Backend/src/models/FriendReqest.js

--- Backend/src/Routes/user.route.js
+++ Backend/src/Routes/user.route.js
@@ -1,12 +1,25 @@
 import express from "express"
 import { protectRoute } from "../middleware/auth.middlware.js";
-import { getRecommendedUser } from "../controllers/user.controller.js";
-import { getMyFriends } from "../controllers/user.controller.js";
+import {
+  acceptFriendRequest,
+  getFriendRequests,
+  getMyFriends,
+  getOutgoingFriendReqs,
+  getRecommendedUsers,
+  sendFriendRequest,
+} from "../controllers/user.controller.js";
+
 const router = express.Router();
 
 router.use(protectRoute);
-router.get("/",getRecommendedUser);
+router.get("/", getRecommendedUsers);
 router.get("/friends", getMyFriends);
+
+router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
+
+router.get("/friend-requests", getFriendRequests);
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
 
 
 
